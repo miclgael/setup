47523afd918a0e416a7d77971097206b27c914b2
Michael Gale <michael@michaelgale.dev>
2023-09-04 08:13:13 +1000

    fix: older homebrew loads from different location

--- dotfiles/modules/sources.sh
+++ dotfiles/modules/sources.sh
@@ -8,11 +8,14 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 # The next line updates PATH for Netlify's Git Credential Helper.
 test -f '~/Library/Preferences/netlify/helper/path.zsh.inc' && source '~/Library/Preferences/netlify/helper/path.zsh.inc'
 
-# Source Spaceshipt prompt. Installed with `brew install spaceship`.
+# Source Spaceshipt prompt. Installed with `brew install spaceship`. Load from either directory.
 test -e /usr/local/opt/spaceship/spaceship.zsh && source /usr/local/opt/spaceship/spaceship.zsh
+test -e /opt/homebrew/opt/spaceship/spaceship.zsh && source /opt/homebrew/opt/spaceship/spaceship.zsh
 
-# Source zsh-autosuggestions. Installed with `brew install zsh-autosuggestions`.
+# Source zsh-autosuggestions. Installed with `brew install zsh-autosuggestions`. Load from either directory.
+test -e /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 test -e /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh && source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-# Source zsh-syntax-highlighting. Installed with `brew install zsh-syntax-highlighting`.
+# Source zsh-syntax-highlighting. Installed with `brew install zsh-syntax-highlighting`. Load from either directory.
+test -e /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 test -e /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
