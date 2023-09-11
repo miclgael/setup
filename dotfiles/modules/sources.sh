725ecfcf11e4752cf7639aa36caf8d4e6e3d921a
Michael Gale <michael@michaelgale.dev>
2023-09-11 15:10:10 +1000

    docs: improve docs

--- dotfiles/modules/sources.sh
+++ dotfiles/modules/sources.sh
@@ -5,7 +5,7 @@
 # integrate iTerm2 helpers directly into zsh
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-# The next line updates PATH for Netlify's Git Credential Helper.
+# The next line updates PATH for Netlify's Git Credential Helper. Installed with `brew install netlify-cli` and `netlify login`.
 test -f "${HOME}/Library/Preferences/netlify/helper/path.zsh.inc" && source "${HOME}/Library/Preferences/netlify/helper/path.zsh.inc"
 
 # Source Spaceshipt prompt. Installed with `brew install spaceship`. Load from either directory.
