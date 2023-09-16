e7b2dca24ece5ac48963625f75e03fb17e111c00
= <=>
2023-09-16 16:52:25 +1000

    chore: sparkles!

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,19 +3,19 @@
 # Abs path to custom dotfiles configs
 SCRIPTS_DIR="$HOME/Code/projects/setup/scripts"
 
-# Required
+# # Required
 source $SCRIPTS_DIR/modules/helpers.sh
-source $SCRIPTS_DIR/modules/setup.sh
+# source $SCRIPTS_DIR/modules/setup.sh
 
-# Modules
-source $SCRIPTS_DIR/modules/brew.sh
-source $SCRIPTS_DIR/modules/casks.sh
-source $SCRIPTS_DIR/modules/node.sh
-source $SCRIPTS_DIR/modules/macos.sh
-source $SCRIPTS_DIR/modules/dock.sh
+# # Modules
+# source $SCRIPTS_DIR/modules/brew.sh
+# source $SCRIPTS_DIR/modules/casks.sh
+# source $SCRIPTS_DIR/modules/node.sh
+# source $SCRIPTS_DIR/modules/macos.sh
+# source $SCRIPTS_DIR/modules/dock.sh
 
 finish_date=$(date +"%Y-%m-%d")
 finish_time_am_pm=$(date +"%I:%M %p")
 
-end_credits "@miclgael macOS setup is complete. \nStarted: ${the_time_am_pm}. Finished: ${finish_time_am_pm}."
+end_credits "@miclgael macOS setup is complete. ✨ \nStarted: ${the_time_am_pm}. Finished: ${finish_time_am_pm}."
 warning "Please log any issues found at https://github.com/miclgael/setup/issues"
