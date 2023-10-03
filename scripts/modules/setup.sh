5a2b30c4a61d07565403bd5f7ac50c58f64dc31e
Michael Gale (he/him) <michael@michaelgale.dev>
2023-10-03 19:58:20 +1100

    chore: tweak time estimation

--- scripts/modules/setup.sh
+++ scripts/modules/setup.sh
@@ -14,7 +14,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-info "Thanks! First time round, this script will take about 35 minutes to complete. You can go grab a coffee or something.\n"
+info "Thanks! First time round, this script can take ~35-70 minutes to complete. After the next step, you can go grab a coffee or something.\n"
 
 sleep 5
 
